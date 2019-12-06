94e3972dad8bd675a812247485c673d4579d7c0c
Brandon Cox <brandon.cox@pm.me>
2019-12-06 11:58:18 -0500

    reuse on day1

--- day1/index.js
+++ day1/index.js
@@ -3,7 +3,7 @@ const parse = require("../dataLoader").parse;
 const day1part1 = (data = []) => {
   data = data.split("\n").map(Number);
   return data.reduce((acc, item) => {
-    const fuelNeeded = Math.floor((item / 3)) - 2;
+    const fuelNeeded = calculateFuel(item);
     return fuelNeeded > 0 ? acc + fuelNeeded : acc;
   }, 0);
 }
@@ -11,17 +11,17 @@ const day1part1 = (data = []) => {
 const day1part2 = (data) => {
   data = data.split("\n").map(Number);
   return data.reduce((acc, item) => {
-    const fuelNeeded = calculateFuel(item);
+    const fuelNeeded = calculateFuel(item, true);
     return fuelNeeded > 0 ? acc + fuelNeeded : acc;
   }, 0);
 }
 
-const calculateFuel = (fuel) => {
+const calculateFuel = (fuel, fuelForFuel = false) => {
   if ((Math.floor((fuel / 3)) - 2) <= 0) {
     return 0;
   }
   const fuelNeeded = Math.floor((fuel / 3)) - 2;
-  return fuelNeeded + calculateFuel(fuelNeeded);
+  return fuelForFuel ? fuelNeeded + calculateFuel(fuelNeeded, true) : fuelNeeded;
 }
 
 const data = parse(`${__dirname}/data.txt`);
