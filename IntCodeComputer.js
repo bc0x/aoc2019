70e21f6173d18855833c8cab657c22823b031c73
Brandon Cox <brandon.cox@pm.me>
2019-12-09 17:36:20 -0500

    last clean up

--- IntCodeComputer.js
+++ IntCodeComputer.js
@@ -8,8 +8,9 @@ const createComputer = data => {
     calculate(opCode, x, y, pointer) {
       if (opCode === 1) return x + y;
       if (opCode === 2) return x * y;
-      if (opCode === 5) return x !== 0 ? y : pointer;
-      if (opCode === 6) return x === 0 ? y : pointer;
+      if (opCode === 3) return this.inputs.shift();
+      if (opCode === 5) return x !== 0 ? y : false;
+      if (opCode === 6) return x === 0 ? y : false;
       if (opCode === 7) return x < y ? 1 : 0;
       if (opCode === 8) return x == y ? 1 : 0;
     },
@@ -26,11 +27,9 @@ const createComputer = data => {
         mode3: Boolean(Number(instructionArray[0])),
       };
     },
-    getParams(opCode) {
+    getParams(numParams) {
       const arr = this.data.slice(this.pointer);
-      if ([1, 2, 7, 8].includes(opCode)) this.pointer += 3;
-      if ([3, 4].includes(opCode)) this.pointer += 1;
-      if ([5, 6].includes(opCode)) this.pointer += 2;
+      this.pointer += numParams;
       return arr;
     },
     compute(inputValue) {
@@ -38,27 +37,23 @@ const createComputer = data => {
       while (this.pointer < this.data.length) {
         const { opCode, mode1, mode2 } = this.getInstruction();
         if ([1, 2, 7, 8].includes(opCode)) {
-          const [inputIdx1, inputIdx2, outputIdx] = this.getParams(opCode);
-          this.data[outputIdx] = this.calculate(
-            opCode,
-            mode1 ? inputIdx1 : this.data[inputIdx1],
-            mode2 ? inputIdx2 : this.data[inputIdx2]
-          );
+          const [inputIdx1, inputIdx2, outputIdx] = this.getParams(3);
+          const param1 = mode1 ? inputIdx1 : this.data[inputIdx1];
+          const param2 = mode2 ? inputIdx2 : this.data[inputIdx2];
+          this.data[outputIdx] = this.calculate(opCode, param1, param2);
         } else if (opCode === 3) {
-          const [outputIdx] = this.getParams(opCode);
-          this.data[outputIdx] = this.inputs.shift();
+          const [outputIdx] = this.getParams(1);
+          this.data[outputIdx] = this.calculate(opCode);
         } else if (opCode === 4) {
-          const [outputIdx] = this.getParams(opCode);
+          const [outputIdx] = this.getParams(1);
           this.output = mode1 ? outputIdx : this.data[outputIdx];
           return this.output;
         } else if ([5, 6].includes(opCode)) {
-          const [inputIdx1, inputIdx2] = this.getParams(opCode);
-          this.pointer = this.calculate(
-            opCode,
-            mode1 ? inputIdx1 : this.data[inputIdx1],
-            mode2 ? inputIdx2 : this.data[inputIdx2],
-            this.pointer
-          );
+          const [inputIdx1, inputIdx2] = this.getParams(2);
+          const param1 = mode1 ? inputIdx1 : this.data[inputIdx1];
+          const param2 = mode2 ? inputIdx2 : this.data[inputIdx2];
+          this.pointer = this.calculate(opCode, param1, param2) || this.pointer;
+          this;
         } else if (opCode === 99) {
           this.halted = true;
           return this.output;
